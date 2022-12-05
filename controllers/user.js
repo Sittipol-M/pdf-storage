986109998307b875a8c95bf543a21566f53102a9
sittipolDAppMaker <sittipol@dappmaker.co.th>
2022-12-05 15:59:46 +0700

    create user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,14 @@
 /** @format */
 
-const express = require("express");
-const router = express.Router();
+const UserService = require("../services/userService");
 
-exports.userRegister = () => {
-  console.log("test");
+exports.userRegister = async (req, res) => {
+  try {
+    const userRegisterData = req.body;
+    const userServiceInstance = new UserService();
+    const user = await userServiceInstance.register(userRegisterData);
+    res.send(user);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 };
