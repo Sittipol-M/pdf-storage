806bdee4ad81c98596cc1d3b229d07c973554308
sittipolDAppMaker <sittipol@dappmaker.co.th>
2022-12-05 20:02:44 +0700

    add body parser to expressLoader

--- shared/loaders/expressLoader.js
+++ shared/loaders/expressLoader.js
@@ -1,7 +1,9 @@
 /** @format */
 
 const routes = require("../.././routes/routes");
-
+const bodyParser = require("body-parser");
 exports.expressLoader = (app) => {
+  // parse application/json
+  app.use(bodyParser.json());
   app.use("/v1/api", routes);
 };
