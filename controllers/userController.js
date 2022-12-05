61592694e51d73d22f47e00edcb120de9268242b
sittipolDAppMaker <sittipol@dappmaker.co.th>
2022-12-05 20:01:38 +0700

    change name user to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,11 +4,19 @@ const UserService = require("../services/userService");
 
 exports.userRegister = async (req, res) => {
   try {
-    const userRegisterData = req.body;
+    const registerData = req.body;
     const userServiceInstance = new UserService();
-    const user = await userServiceInstance.register(userRegisterData);
+    const user = await userServiceInstance.register(registerData);
     res.send(user);
   } catch (error) {
     res.status(400).send({ error: error.message });
+  }
+};
+
+exports.userLogin = async (req, res) => {
+  try {
+    c
+  } catch (error) {
+    res.send(400).send({ error: error.message });
   }
 };
